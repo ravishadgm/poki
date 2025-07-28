7d8245295b80ba8e6d5cf2836c81e2eda08aefdf
ravishadgm <ravishapatel.dgm@gmail.com>
2025-07-28 17:36:18 +0530

    images changes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,77 +1,20 @@
 import Logo from "./Header/Poki_Logo.png";
 import SmallLogo from "./Header/SmallLogo.png";
-import Game1 from "./Header/Game1.webp";
-import Game2 from "./Header/Game2.webp";
-import Game3 from "./Header/Game3.webp";
-import Game4 from "./Header/Game4.webp";
-import USA from "./Footer/usa.svg";
-import Zombie from "./Games/Zombie.webp";
-import GameCard1 from "./Games/GameType1.png";
-import GameCard2 from "./Games/GameType2.png";
-import GameCard3 from "./Games/GameType3.webp";
 
-import GameCard5 from "./Games/GameType5.jpg";
-import GameCard6 from "./Games/GameType6.jpg";
 import GameCard7 from "./Games/smallAdd.jpeg";
-import smallCard1 from "./Games/smallCard/smallCard1.webp";
-import smallCard2 from "./Games/smallCard/smallCard2.webp";
-import smallCard3 from "./Games/smallCard/smallCard3.webp";
-import smallCard4 from "./Games/smallCard/smallCard4.webp";
-import smallCard5 from "./Games/smallCard/smallCard5.webp";
-import smallCard6 from "./Games/smallCard/smallCard6.webp";
-import smallCard7 from "./Games/smallCard/smallCard7.webp";
-import smallCard8 from "./Games/smallCard/smallCard8.webp";
-import smallCard9 from "./Games/smallCard/smallCard9.webp";
-import smallCard10 from "./Games/smallCard/smallCard10.webp";
-import smallCard11 from "./Games/smallCard/smallCard11.webp";
-import smallCard12 from "./Games/smallCard/smallCard12.webp";
-import smallCard13 from "./Games/smallCard/smallCard13.webp";
-import smallCard14 from "./Games/smallCard/smallCard14.webp";
-import smallCard15 from "./Games/smallCard/smallCard15.webp";
-import smallCard16 from "./Games/smallCard/smallCard16.webp";
-import popularImg1 from "./Header/PopularImage/Game4.webp";
-import popularImg2 from "./Header/PopularImage/Game6.webp";
-import popularImg3 from "./Header/PopularImage/Game7.webp";
-import popularImg4 from "./Header/PopularImage/Game5.webp";
+
+
 import privacyHeader from "./Privacy/header-bg.svg";
 import privacyFooter from "./Privacy/footer-bg.svg";
 
 const images = {
     Logo,
     SmallLogo,
-    Game1,
-    Game2,
-    Game3,
-    Game4,
-    USA,
-    Zombie,
-    GameCard1,
-    GameCard2,
-    GameCard3,
-
-    GameCard5,
-    GameCard6,
+ 
+ 
     GameCard7,
-    smallCard1,
-    smallCard2,
-    smallCard3,
-    smallCard4,
-    smallCard5,
-    smallCard6,
-    smallCard7,
-    smallCard8,
-    smallCard9,
-    smallCard10,
-    smallCard11,
-    smallCard12,
-    smallCard13,
-    smallCard14,
-    smallCard15,
-    smallCard16,
-    popularImg1,
-    popularImg2,
-    popularImg3,
-    popularImg4,
+   
+    
     privacyHeader,
     privacyFooter
 }
