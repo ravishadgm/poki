853167641b7b1e1185b3e35b9f434bedd40350d7
ravishadgm <ravishapatel.dgm@gmail.com>
2025-07-29 14:15:39 +0530

    changes

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -1,5 +1,4 @@
 import PrivacyInfo from "@/components/PrivacyInfo/PrivacyInfo";
-import styles from "./styles.module.scss";
 import GetInTouch from "@/components/GetInTouch/GetInTouch";
 import PrivacyHeader from "@/layout/PrivacyHeader/PrivacyHeader";
 
