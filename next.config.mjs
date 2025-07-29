06679f69c62153df1cdec70eddd3e60e15955c11
ravishadgm <ravishapatel.dgm@gmail.com>
2025-07-29 12:26:54 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,9 @@ const nextConfig = {
     `,
   },
   env: {
-    CUSTOM_BASE_URL: process.env.NODE_ENV === 'development' 
-      ? 'http://localhost:3000'
-      : process.env.NEXT_PUBLIC_BASE_URL || 'https://poki-tan.vercel.app'
-  }
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://poki-tan.vercel.app'
+}
+
 
 }
 
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,5 +1,5 @@
 export async function getGames() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const timestamp = Date.now();
 
   const res = await fetch(`${baseUrl}/data/games.json?t=${timestamp}`, {
